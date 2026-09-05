6eb6223fd6b7232d064ea007b5c574214c709b91
AliceUwase <a.uwase3@alustudent.com>
2026-09-05 02:10:59 +0200

    feat: Add Teacher interface and printTeacher function

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Define the Teacher interface with the specified properties
 interface Teacher {
     firstName: string;
     lastName: string;
@@ -5,4 +6,15 @@ interface Teacher {
     yearsOfExperience: number;
     location: string;
     [key: string]: string | number | boolean;
+}
+
+// function to return the first letter of the firstName and the full lastName
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+// Define the type for the printTeacher function
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+    
 }
