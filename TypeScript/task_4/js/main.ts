eaa69064ee4ee47d5391911777b39bab106d9f3d
AliceUwase <a.uwase3@alustudent.com>
2026-09-13 23:41:01 +0200

    feat: add main.ts file with namespace and subject references

---
+++ TypeScript/task_4/js/main.ts
@@ -0,0 +1,6 @@
+// namespace Subjects for main application
+/// <reference path="subjects/Teacher.ts" />
+/// <reference path="subjects/Subject.ts" />
+/// <reference path="subjects/Cpp.ts" />
+/// <reference path="subjects/React.ts" />
+/// <reference path="subjects/Java.ts" />
