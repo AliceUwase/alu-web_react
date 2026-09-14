f92273c4c0c000a5893c16f282a9ae3d673a615e
AliceUwase <a.uwase3@alustudent.com>
2026-09-14 02:27:39 +0200

    feat: initialize dashboard project with create-react-app structure

---
+++ react_intro/task_1/dashboard/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Notifications} from './Notifications';
+
+// Create a div for the notifications
+const notificationsDiv = document.createElement('div');
+notificationsDiv.id = 'root-notifications';
+document.body.appendChild(notificationsDiv);
+
+// Render the Notifications component in the new div
+const notificationsRoot = ReactDOM.createRoot(document.getElementById('root-notifications'));
+notificationsRoot.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
