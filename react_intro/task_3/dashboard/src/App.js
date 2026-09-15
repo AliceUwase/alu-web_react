9db255735ae7056c7a79565ae272f6f54485528e
AliceUwase <a.uwase3@alustudent.com>
2026-09-15 14:54:58 +0200

    feat: Add App component and  styles

---
+++ react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,39 @@
+import logo from './h-logo.jpg';
+import { getFullYear, getFooterCopy } from "./utils.js";
+import React from 'react';
+import { Notifications } from './Notifications';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Notifications />
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School Dashboard</h1>
+      </header>
+
+      <main className="App-body">
+        <p>
+          Login to access the full Dashboard.
+        </p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <button type="submit">OK</button>
+        </form>
+      </main>
+      <footer className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(false)}.
+        </p>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
